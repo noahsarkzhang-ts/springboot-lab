b5099a78131ba867acc3a203d09341b7507de936
allen <zhangxt@fsmeeting.com>
2022-02-22 20:03:52 +0800

    添加 springsecurity oauth2 jwt 支持

---
+++ springboot-oauth2/authorization-server-oauth2-jwt/src/main/java/org/noahsark/AuthorizationServerJwtApplication.java
@@ -0,0 +1,13 @@
+package org.noahsark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationServerJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( AuthorizationServerJwtApplication.class, args );
+    }
+
+}
